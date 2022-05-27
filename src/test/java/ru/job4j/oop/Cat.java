afa8a339294f8d22c08d1e2982b5d88bbfd8f0bc
G4lex <ganalex01@gmail.com>
2022-05-28 00:23:12 +0300

    cat 2

--- src/test/java/ru/job4j/oop/Cat.java
+++ src/test/java/ru/job4j/oop/Cat.java
@@ -1,6 +1,24 @@
 package ru.job4j.oop;
 
 public class Cat {
+
+    private String name;
+
+    private String food;
+
+    public void show() {
+        System.out.println(this.food);
+        System.out.println(this.name);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
     public String sound() {
         String voice = "may-may";
         return voice;
@@ -11,5 +29,14 @@ public class Cat {
         Cat sparky = new Cat();
         String say = peppy.sound();
         System.out.println("Peppy says " + say);
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick("Gav");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveNick("Black");
+        black.show();
     }
 }

ca92578d8770311f7c025888ef9215a4619e5b5a
G4lex <ganalex01@gmail.com>
2022-06-02 18:14:53 +0300

    triangle oop2

--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -3,6 +3,7 @@ package ru.job4j.oop;
 import static org.hamcrest.Matchers.closeTo;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TriangleTest {
@@ -15,5 +16,16 @@ public class TriangleTest {
         Triangle triangle = new Triangle(a, b, c);
         double rsl = triangle.area();
         assertThat(rsl, closeTo(8, 0.001));
+    }
+
+    @Test
+    public void notExistThenminus1() {
+        Point a = new Point(1, 0);
+        Point b = new Point(0, 0);
+        Point c = new Point(0, 0);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        double result = -1;
+        assertThat(rsl, closeTo(-1, 0.01));
     }
 }

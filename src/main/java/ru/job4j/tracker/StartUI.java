42bac382ab0573483ce400dd60d7999455e10a17
G4lex <ganalex01@gmail.com>
2022-06-04 23:59:52 +0300

    item геттер весь мозг..вы

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,14 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+
+    public static void main(String[] args) {
+        Item data = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String dataform = data.getCreated().format(formatter);
+        System.out.println("Текущая дата и время: " + dataform);
+    }
+}

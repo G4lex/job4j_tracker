bdcd2601b7ec68bba9d530f0f8a09bd75209989a
G4lex <ganalex01@gmail.com>
2022-06-09 00:04:22 +0300

    9. toString [#310018]валидация? [#246864]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -10,5 +10,6 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String dataform = data.getCreated().format(formatter);
         System.out.println("Текущая дата и время: " + dataform);
+        System.out.println(data);
     }
 }
